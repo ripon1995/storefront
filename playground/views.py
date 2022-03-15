46cf642852ab0f6989b3951da2a0f08ef4a7c0bc
DESKTOP-PSR46S4\BS592 <ripon1995dc@gmail.com>
2022-03-15 19:38:58 +0600

    admin page added

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
 
 # Create your views here.
 def say_hello(request) :
-    return HttpResponse('Hello world')
+    products = Product.objects.filter(unit_price__range=(20,30))
+    return HttpResponse(list(products))
