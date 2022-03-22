9816951b7dd6478a9d8a3e532c86290e2e32572f
DESKTOP-PSR46S4\BS592 <ripon1995dc@gmail.com>
2022-03-22 13:25:18 +0600

    generic views implemented for ProductList and CollectionList in different way

--- store/views.py
+++ store/views.py
@@ -1,27 +1,24 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
 from rest_framework import status
 from .models import Collection, Product
 from .serializers import CollectionSerializer, ProductSerializer
 # Create your views here.
 
 
-class ProductList(APIView):
+class ProductList(ListCreateAPIView):
+    # if we have complex querySet then we have to implement that in get_queryset() method
+    def get_queryset(self):
+        return Product.objects.all()
 
-    def get(self, request):
-        querySet = Product.objects.all()
-        serializer = ProductSerializer(
-            querySet,
-            many=True,
-            context={'request': request})
-        return Response(serializer.data)
+    # if we have complex serializer then we have to implement that in get_serializer_class
+    def get_serializer_class(self):
+        return ProductSerializer
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
 class ProductDetail(APIView):
@@ -45,21 +42,14 @@ class ProductDetail(APIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+# this can be done like ProductList but as there is no complex query and serializer we can simply
+# create this attributes
 
-class CollectionList(APIView):
-    def get(self, request):
-        querysets = Collection.objects.all()
-        serializer = CollectionSerializer(
-            querysets,
-            many=True
-        )
-        return Response(serializer.data)
 
-    def post(self, request):
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CollectionList(ListCreateAPIView):
+
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
 
 
 class CollectionDetail(APIView):
